840633de9f027abcd4344454a1d3acad700f000b
BuildTools <unconfigured@null.spigotmc.org>
2024-09-21 01:15:49 -0400

    yes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,18 @@
 import NavbarComponent from './components/NavbarComponent.jsx';
+import {MarqueeDemo} from "./components/MarqueeDemo.jsx"
+import Cards from "./components/Cards.jsx"
+import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
 
 function App() {
 
   return (
       <div className="tw-bg-gradient-to-r tw-from-blue-950 tw-to-blue-200 tw-min-h-screen tw-w-screen">
-          <NavbarComponent />
+          <NavbarComponent/>
+          <MarqueeDemo />
+          <Cards />
+          <Cards />
+          <Cards />
       </div>
   )
 }
