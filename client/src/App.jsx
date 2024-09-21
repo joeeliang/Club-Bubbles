176f45d0a640d48e243df105656696ca04820308
Nitin S <nitin.satul@gmail.com>
2024-09-21 15:50:12 -0400

    signup route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import GradualSpacing from "@/components/magicui/gradual-spacing";
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
-
+import Signup from './Pages/Signup.jsx';
 import Browse from './Pages/Browse.jsx';
 import Join from './Pages/Join.jsx';
 import Login from './Pages/Login.jsx';
@@ -22,10 +22,12 @@ function App() {
                 <NavbarComponent/>
 
                 <Routes>
-                    <Route path="/" element={<PageWrapper/>}/>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/home" element={<PageWrapper/>}/>
                     <Route path="/browse" element={<Browse/>}/>
                 <Route path="/join" element={<Join/>}/>
                 <Route path="/login" element={<Login/>}/>
+                <Route path="/Signup" element = {<Signup />} />
                 </Routes>
             </div>
         </Router>
@@ -37,7 +39,7 @@ function PageWrapper({ content }) {
 
     return (
         <>
-            {location.pathname === '/' && (
+            {location.pathname === '/home' && (
                 <>
                     <DotPattern
                         className={cn(
@@ -46,7 +48,7 @@ function PageWrapper({ content }) {
                     />
                 <GradualSpacing
                     className="tw-font-display tw-text-center tw-text-6xl tw-font-bold tw-tracking-[-0.1em] tw-text-black tw-dark:text-white tw-md:text-7xl tw-md:leading-[5rem] tw-my-4"
-                    text="Browsing"
+                    text="Home"
                 />
                 <MarqueeDemo />
                 </>
